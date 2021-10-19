5e25a08fa21b3184f6a0ada6076102767bc65217
jwright90 <jason.wright90@gmail.com>
2021-10-19 13:10:03 +0100

    Next Link & Router

--- pages/about.js
+++ pages/about.js
@@ -12,6 +12,7 @@ export default function AboutPage() {
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events.</p>
       <p>Version: 1.0.0</p>
+      <Link href="/">Home</Link>
     </div>
   );
 }
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import styles from '../../styles/Home.module.css';
 
 export default function EventPage() {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,6 +12,8 @@ export default function EventPage() {
       </Head>
 
       <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push('/')}>Go Home</button>
     </div>
   );
 }
